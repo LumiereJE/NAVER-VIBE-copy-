cb34e1420a780df5f1379d490bf46d76880d2bf8
lumiereJE <lumiere_remplir@naver.com>
2024-03-23 01:04:00 +0900

    애니메이션 정지버튼, 인트로 추가

--- naver_vibe_info.js
+++ naver_vibe_info.js
@@ -1,3 +1,4 @@
+// 소개 페이지 - 1번 section bar width 100%
 function bar_100() {
   const bars = document.querySelectorAll(".bar");
 
@@ -7,6 +8,7 @@ function bar_100() {
 }
 setTimeout(bar_100, 500);
 
+// 소개 페이지 - 1번 section bar 이후 글자 올라오기
 function word_up() {
   const elements = document.querySelectorAll(".word, .install_btn");
 
@@ -15,3 +17,24 @@ function word_up() {
   });
 }
 setTimeout(word_up, 1300);
+
+// 소개 페이지 - 2번 section animation 정지 / 재생
+const playPauseButton = document.querySelector(".play_btn_box");
+const btnOn = document.querySelector(".btn_on");
+const btnOff = document.querySelector(".btn_off");
+const playlistBox = document.querySelector(".playlist_box");
+let isPaused = false;
+
+playPauseButton.addEventListener("click", function () {
+  if (isPaused) {
+    playlistBox.classList.remove("paused");
+    btnOn.style.display = "block";
+    btnOff.style.display = "none";
+    isPaused = false;
+  } else {
+    playlistBox.classList.add("paused");
+    btnOn.style.display = "none";
+    btnOff.style.display = "block";
+    isPaused = true;
+  }
+});
